a83f726b258e9ef09aeeecf102b22093147582c5
veryDeepWell <145202229+veryDeepWell@users.noreply.github.com>
2026-09-18 13:01:24 +0300

    Hub restore parts 1-4 + boot(5) + hub html + common sort cycle

--- tarkov-hub-app.js
+++ tarkov-hub-app.js
@@ -1,7 +1,7 @@
 /*! hub-app assemble */
 (function () {
   var a = window.__HUB_B64;
-  if (!a || a.filter(Boolean).length < 3) {
+  if (!a || a.filter(Boolean).length < 5) {
     console.error("hub-app parts missing", a && a.length);
     return;
   }
